825919dd0c7b0efe618006dcf9f91a4a609fe4c7
Lawrence Jackson <l.jackson96@outlook.com>
2020-02-05 19:12:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number = 1
+second_number = 2
+expect (second_number).not_to equal(0)
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
